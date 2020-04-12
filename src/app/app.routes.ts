6c56bb5f2c574ae4b15aa6e07268c5b22be6ed62
Mikhail.D <Mikhail.d@cronix.ms>
2020-04-12 21:52:49 +0300

    adding x4calc prefix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,42 +1,48 @@
-import { Routes } from '@angular/router';
-import { AboutComponent } from './about.component';
+import {Routes} from '@angular/router';
+import {AboutComponent} from './about.component';
 
 export const appRoutes: Routes = [
-   {
-      path: '',
-      redirectTo: 'station-calculator',
-      pathMatch: 'full'
-   },
-   {
-      path: 'about',
-      component: AboutComponent
-   },
-   {
-      path: 'ships',
-      loadChildren: './ship/ships.module#ShipsModule'
-   },
-   {
-      path: 'equipment',
-      loadChildren: './equipment/equipment.module#EquipmentModule'
-   },
-   {
-      path: 'factions',
-      loadChildren: './faction/faction.module#FactionModule'
-   },
-   {
-      path: 'races',
-      loadChildren: './races/race.module#RaceModule'
-   },
-   {
-      path: 'modules',
-      loadChildren: './module/module.module#ModuleModule'
-   },
-   {
-      path: 'station-calculator',
-      loadChildren: './station/station.module#StationModule'
-   },
-   {
-      path: 'wares',
-      loadChildren: './ware/ware.module#WareModule'
-   }
+    {path: '', redirectTo: 'x4calc/station-calculator', pathMatch: 'full'},
+    {
+        path: 'x4calc', children: [
+            {
+                path: '',
+                redirectTo: 'x4calc/station-calculator',
+                pathMatch: 'full'
+            },
+            {
+                path: 'about',
+                component: AboutComponent
+            },
+            {
+                path: 'ships',
+                loadChildren: './ship/ships.module#ShipsModule'
+            },
+            {
+                path: 'equipment',
+                loadChildren: './equipment/equipment.module#EquipmentModule'
+            },
+            {
+                path: 'factions',
+                loadChildren: './faction/faction.module#FactionModule'
+            },
+            {
+                path: 'races',
+                loadChildren: './races/race.module#RaceModule'
+            },
+            {
+                path: 'modules',
+                loadChildren: './module/module.module#ModuleModule'
+            },
+            {
+                path: 'station-calculator',
+                loadChildren: './station/station.module#StationModule'
+            },
+            {
+                path: 'wares',
+                loadChildren: './ware/ware.module#WareModule'
+            }
+        ]
+    },
+    {path: '**', redirectTo: 'x4calc/station-calculator', pathMatch: 'full'},
 ];
